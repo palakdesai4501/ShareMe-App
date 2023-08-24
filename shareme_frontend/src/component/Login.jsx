a027914433501cae5f6cdf5b79a68c515f794b5d
Palak <palak.desai4501@gmail.com>
2023-08-24 13:37:43 +0530

    Feature: Added onClick and disabled Properties of renderProps

--- shareme_frontend/src/component/Login.jsx
+++ shareme_frontend/src/component/Login.jsx
@@ -28,7 +28,9 @@ const Login = () => {
                             render={(renderProps) => (
                                 <button
                                     type="button"
-                                    className="bg-mainColor flex justify-center items-center p-3 rounded-lg"
+                                    className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                                    onClick={renderProps.onClick}
+                                    disabled={renderProps.disabled}
                                 >
                                     <FcGoogle className="mr-4" />Sign in with google
                                 </button>
