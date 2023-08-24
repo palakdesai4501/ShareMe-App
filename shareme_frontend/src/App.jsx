a91eab1297abbe4ae6ce427c52fee7fbdbd44fc4
Palak <palak.desai4501@gmail.com>
2023-08-24 10:35:34 +0530

    Feature: Added Login and Home Components in Component and Conatiner Folder Respectively

--- shareme_frontend/src/App.jsx
+++ shareme_frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Login from './component/Login';
+import Home from './container/Home';
 
 const App = () => {
   return (
-    <div>
-      Hello world
-    </div>
-  ) 
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
+  )
 }
 
 export default App
