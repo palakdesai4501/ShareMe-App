3223da030f47ab69c9a08b847bd06a07ea8230b9
Palak <palak.desai4501@gmail.com>
2023-08-25 16:22:09 +0530

    Feature: Added Project And Token Id Directly

--- shareme_frontend/src/client.jsx
+++ shareme_frontend/src/client.jsx
@@ -2,11 +2,11 @@ import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+    projectId: '5por7u6w',
     dataset: 'production',
     apiVersion: '2021-11-16',
     useCdn: true,
-    token: process.env.REACT_APP_SANITY_TOKEN,
+    token: 'skXsCDeSW0bpDPLxBylGjugNw6RgI7vD4fxf4IG3UzIRQLW0zi81bsrHVWs1u7sPob1zTWGzbJZhix9LQRp7c1HImReNSJpO08WTWaWSUE922bG2PWpMRtw1cRdBEuFhgQSH3IGSIqasX8FR07BtOcVhTeJoN5jGHf2P1WAZRakOlTm9J2SN',
 });
 
 const builder = imageUrlBuilder(client);
